7e939555a97653cf1a39f348013d9cfee0de1c6a
lab lab <lab@labinfo.is.escuelaing.edu.co>
2024-10-19 09:46:34 -0500

    HELLO CONTROLLER WORKING

--- src/main/java/com/escuelaing/edu/co/distribuited_systems/DistribuitedSystemsApplication.java
+++ src/main/java/com/escuelaing/edu/co/distribuited_systems/DistribuitedSystemsApplication.java
@@ -3,11 +3,22 @@ package com.escuelaing.edu.co.distribuited_systems;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class DistribuitedSystemsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DistribuitedSystemsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(DistribuitedSystemsApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", getPort()));
+        app.run(args);
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 33025;
+    }
 
 }
