8e98cee2b2d911415212ef3371900d50d54b2dcf
lab lab <lab@labinfo.is.escuelaing.edu.co>
2024-10-19 09:39:23 -0500

    base proyect

---
+++ src/main/java/com/escuelaing/edu/co/distribuited_systems/DistribuitedSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.escuelaing.edu.co.distribuited_systems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistribuitedSystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistribuitedSystemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/escuelaing/edu/co/distribuited_systems/DistribuitedSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escuelaing.edu.co.distribuited_systems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistribuitedSystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
