95487050ab0ceecc6e74e9ffe7e8cce2bff35c81
Ivan Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2024-10-20 15:12:30 -0500

    agregando servidor de eureka

---
+++ 02-servicio-eureka-server/src/test/java/com/escuelaing/edu/co/distribuited_systems/eureka/servicio_eureka_server/ServicioEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escuelaing.edu.co.distribuited_systems.eureka.servicio_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
