3737de805fbd069b3f15c474bcb4b2d9beeb7341
Ivan Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2024-10-20 16:23:33 -0500

    ENV VARIABLES FRONT END

---
+++ 01-front-end/src/main/java/com/escuelaing/edu/co/distribuited_systems/controllers/UserRestController.java
@@ -0,0 +1,15 @@
+package com.escuelaing.edu.co.distribuited_systems.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Ivan Camilo Rincon Saavedra
+ * @version 1.0
+ * @since 10/20/2024
+ */
+
+@RestController
+@Slf4j
+public class UserRestController {
+}
